276b49fcbf51f26cdcace3b873dc020290f89698
charlotteetmathieutalmont-wq <charlotteetmathieu.talmont@gmail.com>
2026-09-06 15:48:27 +0200

    Update camera.py

--- camera.py
+++ camera.py
@@ -19,6 +19,7 @@ from homeassistant.components.generic.camera import (
 )
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers import device_registry as dr
 from homeassistant.helpers import template
 from homeassistant.helpers.entity import DeviceInfo
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
@@ -196,9 +197,10 @@ class FreeboxHomeNodeCamera(FreeBoxCamera):
             model=CATEGORY_TO_MODEL.get(self._home_node["category"]),
             name=f"{self._home_node['label']}",
             sw_version=fw_version,
-            via_device=(
-                DOMAIN,
-                self._router.mac,
+            via_device_id=dr.async_get_device_id_by_identifier(
+                self.hass,
+                (DOMAIN, self._router.mac),
+                config_entry_id=self._router.config_entry.entry_id,
             ),
             manufacturer="Freebox SAS",
         )
