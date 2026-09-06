078c815412d5a57d9e7e837c0a2866fe6ed45afc
charlotteetmathieutalmont-wq <charlotteetmathieu.talmont@gmail.com>
2026-09-06 16:21:54 +0200

    Update binary_sensor.py

--- binary_sensor.py
+++ binary_sensor.py
@@ -13,7 +13,6 @@ from homeassistant.components.binary_sensor import (
 )
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant, callback
-from homeassistant.helpers import device_registry as dr
 from homeassistant.helpers.dispatcher import async_dispatcher_connect
 from homeassistant.helpers.entity import DeviceInfo
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
@@ -222,11 +221,7 @@ class FreeboxHomeNodeBinarySensor(FreeboxBinarySensor):
             model = CATEGORY_TO_MODEL.get(self._home_node["category"]),
             name = f"{self._home_node['label']}",
             sw_version = str(fw_version),
-            via_device_id=dr.async_get_device_id_by_identifier(
-                self.hass,
-                (DOMAIN, self._router.mac),
-                config_entry_id=self._router.config_entry.entry_id,
-            ),
+            via_device_id=router.device_id,
             manufacturer="Freebox SAS",
         )
 
